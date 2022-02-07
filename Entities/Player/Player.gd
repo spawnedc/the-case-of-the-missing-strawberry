af3b3d6de7257d05c3091962fcb25227b08c4ad7
Can Yilmaz <spawnedc@gmail.com>
2022-02-07 20:29:07 +0000

    better player movement

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,20 +3,28 @@ extends KinematicBody2D
 const SPEED := 200
 
 var velocity = Vector2.ZERO
+var direction = Vector2.ZERO
 
 onready var animated_sprite = $AnimatedSprite
 
 
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if Dialogic.has_current_dialog_node():
+			direction = Vector2.ZERO
+		else:
+			direction.x = (
+				Input.get_action_strength("ui_right")
+				- Input.get_action_strength("ui_left")
+			)
+			direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+			# If input is digital, normalize it for diagonal movement
+			if abs(direction.x) == 1 and abs(direction.y) == 1:
+				direction = direction.normalized()
+
+
 func _physics_process(_delta):
-	# Get player input
-	var direction: Vector2
-	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
-	# If input is digital, normalize it for diagonal movement
-	if abs(direction.x) == 1 and abs(direction.y) == 1:
-		direction = direction.normalized()
-
 	# Apply movement
 	var movement = SPEED * direction
 	velocity = move_and_slide(movement)
